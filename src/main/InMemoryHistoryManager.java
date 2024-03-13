17c8b8de5084bb9e075722cbc332bee0155765f1
RuslanRinatovich <safiullin@yandexlyceum.ru>
2024-03-13 17:20:55 +0300

    Добавить метод removeNode

--- src/main/InMemoryHistoryManager.java
+++ src/main/InMemoryHistoryManager.java
@@ -1,27 +1,26 @@
 package main;
-import java.util.ArrayList;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 import main.models.Node;
 import main.models.Task;
-import java.util.LinkedList;
-import java.util.List;
-
-
 
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-
-
-    //private final List<Task> historyTask = new LinkedList<>();
-
-    private final HandMadeLinkedList historyTask = new HandMadeLinkedList();
+    private Node head;
+    private Node tail;
+    private int size = 0;
+    private final Map<Integer, Node> historyTask = new HashMap<>();
 
     @Override
     public void add(Task task) {
 
-        historyTask.linkLast(task);
+        if (historyTask.containsKey(task.getId()))
+        {
+            removeNode(historyTask.get(task.getId()));
+        }
+        linkLast(task);
+        historyTask.put(task.getId(), tail);
         //historyTask2.linkLast(task);
     }
 
@@ -31,58 +30,51 @@ public class InMemoryHistoryManager implements HistoryManager {
     }
     @Override
     public List<Task> getHistory() {
-        return historyTask.getTasks();
+        return getTasks();
     }
 
-    static class HandMadeLinkedList {
-        /**
-         * Указатель на первый элемент списка. Он же first
-         */
-        private Node head;
-        /**
-         * Указатель на последний элемент списка. Он же last
-         */
-        private Node tail;
-        private int size = 0;
+    public Node linkLast(Task element) {
 
-
-        public void linkLast(Task element) {
-
-            final Node oldTail = tail;
-            final Node newNode = new Node(oldTail, element, null);
-            if (size == 0)
-                head = newNode;
+        final Node oldTail = tail;
+        final Node newNode = new Node(oldTail, element, null);
+        if (size == 0)
+            head = newNode;
+        tail = newNode;
+        if (oldTail == null)
             tail = newNode;
-            if (oldTail == null)
-                tail = newNode;
-            else
-                oldTail.next = newNode;
-            size++;
-        }
-
-        public List<Task> getTasks()
-        {
-            ArrayList<Task> result = new ArrayList<>();
-            Node current = head;
-            while (current != null)
-            {
-                result.add(current.data);
-                current = current.next;
-            }
-            return result;
-        }
-
-        public void removeNode(Node node)
-        {
-            node.prev.next = node.next;
-            node.next.prev = node.prev;
-            node.prev = null;
-            node.next = null;
-        }
-
-
-        public int size() {
-            return this.size;
-        }
+        else
+            oldTail.next = newNode;
+        size++;
+        return newNode;
     }
+
+    public List<Task> getTasks()
+    {
+        ArrayList<Task> result = new ArrayList<>();
+        Node current = head;
+        while (current != null)
+        {
+            result.add(current.data);
+            current = current.next;
+        }
+        return result;
+    }
+
+    public void removeNode(Node node)
+    {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        node.prev = null;
+        node.next = null;
+    }
+
+
+    public int size() {
+        return this.size;
+    }
+
+
+
+
+
 }
